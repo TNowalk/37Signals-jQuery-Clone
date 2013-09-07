3474e65a3bd01aebfd2b0cbaa974360a520b925d
Trevor Nowalk <tnowalk@yahoo.com>
2013-09-07 15:44:09 -0400

    Initial commit of 37Signals jQuery Clone

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,4 +22,13 @@ $(document).ready(function (){
 
   // Set the text of the date message
   $('#dateMessage').text("Happy " + daysOfWeek[dayOfWeek] + ".");
+
+  // Bind hover to links
+  $(".productBox").hover(function() {
+    // This function will fire on mouseover
+    $(".productOverlay." + $(this).attr('data-product')).show();
+  }, function() {
+    // This function will fire on mouseout
+    $(".productOverlay." + $(this).attr('data-product')).hide();
+  })
 });
